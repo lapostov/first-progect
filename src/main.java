e9335452660422adf7bba16209bfbb0c2cffaf18
Александр Масляков <lapostov@yandex.ru>
2022-06-24 17:43:01 +0400

    Added gitignore, changed name issue

--- src/main.java
+++ src/main.java
@@ -14,5 +14,7 @@ public class main {
         System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
         System.out.println((spices / eaters) + " ложек(ка) специй");
 
+
     }
 }
+
